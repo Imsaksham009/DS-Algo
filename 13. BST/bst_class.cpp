187820bf939f7299156b5c515e1f789c0bdda1ad
Saksham Gupta <guptasaksham82@gmail.com>
2022-10-29 16:48:31 +0530

    BST Class

--- 13. BST/bst_class.cpp
+++ 13. BST/bst_class.cpp
@@ -1,158 +1,154 @@
 #include <iostream>
-#include <queue>
+using namespace std;
 
 template <typename T>
-class BinaryTreeNode
-{
-public:
+class BinaryTreeNode {
+   public:
     T data;
     BinaryTreeNode<T> *left;
     BinaryTreeNode<T> *right;
 
-    BinaryTreeNode(T data)
-    {
+    BinaryTreeNode(T data) {
         this->data = data;
         left = NULL;
         right = NULL;
     }
 };
 
-using namespace std;
-
-BinaryTreeNode<int> *takeInput()
-{
-    int rootData;
-    cin >> rootData;
-    if (rootData == -1)
-    {
-        return NULL;
-    }
-    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
-    queue<BinaryTreeNode<int> *> q;
-    q.push(root);
-    while (!q.empty())
-    {
-        BinaryTreeNode<int> *currentNode = q.front();
-        q.pop();
-        int leftChild, rightChild;
-
-        cin >> leftChild;
-        if (leftChild != -1)
+class BST {
+    // Define the data members
+    private:
+    	BinaryTreeNode<int>* root;
+    
+    	//search
+    	bool search(int data, BinaryTreeNode<int>* node)
         {
-            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
-            currentNode->left = leftNode;
-            q.push(leftNode);
+            if(node == NULL)
+                return false;
+            if(node->data == data) return true;
+            else if(data > node->data) return search(data,node->right);
+            else return search(data,node->left);
         }
-
-    cin >> rightChild;
-    if (rightChild != -1)
+    //insertion
+    BinaryTreeNode<int>* insert(int data, BinaryTreeNode<int>* node)
     {
-        BinaryTreeNode<int> *rightNode =
-            new BinaryTreeNode<int>(rightChild);
-        currentNode->right = rightNode;
-        q.push(rightNode);
-    }
-    }
-    return root;
-}
-
-void printLevelATNewLine(BinaryTreeNode<int> *root)
-{
-    queue<BinaryTreeNode<int> *> q;
-    q.push(root);
-    q.push(NULL);
-    while (!q.empty())
-    {
-        BinaryTreeNode<int> *first = q.front();
-        q.pop();
-        if (first == NULL)
+        if(node == NULL)
         {
-            if (q.empty())
-            {
-                break;
-            }
-            cout << endl;
-            q.push(NULL);
-            continue;
+            BinaryTreeNode<int>* newNode = new BinaryTreeNode<int>(data);
+            return newNode;
         }
-        cout << first->data << " ";
-        if (first->left != NULL)
-        {
-            q.push(first->left);
-        }
-        if (first->right != NULL)
-        {
-            q.push(first->right);
-        }
-    }
-}
-
-class BST
-{
-private:
-
-    bool find(int data, BinaryTreeNode<int> *root)
-    {
-        if (root == NULL)
-            return false;
-
-        if (root->data == data)
-            return true;
-
-        if (data > root->data)
-            return find(data, root->right);
-
+        if(data>node->data)
+            node->right = insert(data,node->right);
         else
-            return find(data, root->left);
+            node->left = insert(data,node->left);
+        
+        return node;
+        
     }
-
-public:
-
-    BinaryTreeNode<int> *root;
-
-
-    BST()
+    
+    //deletion
+    BinaryTreeNode<int>* remove(int data,BinaryTreeNode<int>* node)
     {
+		if(node == NULL)
+            return node;
+        if(data>node->data)
+            node->right = remove(data,node->right);
+        else if(data<node->data)
+            node->left = remove(data,node->left);
+        else
+        {
+            if(node->left == NULL && node->right == NULL){
+                delete node;
+                return NULL;
+            }
+            else if(node->left == NULL)
+            {
+                BinaryTreeNode<int>* temp = node->right;
+                delete node;
+                return temp;
+            }
+            else if(node->right == NULL)
+            {
+               	BinaryTreeNode<int>* temp = node->left;
+              	delete node;
+                return temp; 
+            }
+            else
+            {
+                BinaryTreeNode<int>* rmin = node->right;
+                while(rmin->left!=NULL)
+                    rmin = rmin->left;
+                node->data = rmin->data;
+                node->right = remove(rmin->data,node->right);
+                return node;
+            }
+        }
+    }
+    
+    //printing
+    void print(BinaryTreeNode<int>* node)
+    {
+        if(node== NULL)
+            return;
+        cout<<node->data<<":";
+        if(node->left) cout<<"L:"<<node->left->data<<",";
+        if(node->right) cout<<"R:"<<node->right->data;
+        cout<<endl;
+        print(node->left);
+        print(node->right);
+        
+    }
+   public:
+    BST() { 
+        // Implement the Constructor
         root = NULL;
     }
 
-    ~BST()
-    {
-        delete root;
+	/*----------------- Public Functions of BST -----------------*/
+
+    void remove(int data) { 
+        // Implement the remove() function 
+        this->root = remove(data,this->root);
     }
 
-    // void insert(int data)
-    // {
-    // }
+    void print() { 
+        // Implement the print() function
+        print(this->root);
+    }
 
-    // void deleteData(int data)
-    // {
-    //     if (find(data))
-    //     {
-    //     }
-    //     else
-    //     {
-    //         cout << "Data Not Found" << endl;
-    //     }
-    // }
+    void insert(int data) { 
+        // Implement the insert() function 
+        this->root = insert(data,this->root);
+    }
 
-    bool find(int data)
-    {
-        return find(data, this->root);
+    bool search(int data) {
+		// Implement the search() function 
+        return search(data,this->root);
     }
 };
 
-int main()
-{
-    BST b;
-
-    BinaryTreeNode<int>* r = takeInput();
-
-    b.root = r;
-
-    bool ans = b.find(4);
-    cout<<ans<<endl;
-
-
-    return 0;
+int main() {
+    BST *tree = new BST();
+    int choice, input, q;
+    cin >> q;
+    while (q--) {
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cin >> input;
+                tree->insert(input);
+                break;
+            case 2:
+                cin >> input;
+                tree->remove(input);
+                break;
+            case 3:
+                cin >> input;
+                cout << ((tree->search(input)) ? "true\n" : "false\n");
+                break;
+            default:
+                tree->print();
+                break;
+        }
+    }
 }
