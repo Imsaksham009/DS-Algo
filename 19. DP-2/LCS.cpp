e9ab429f4886b28ec760d354e99672f05f8511ce
Saksham Gupta <guptasaksham82@gmail.com>
2022-11-25 16:05:39 +0530

    LCS_Dp(bottomUP)

--- 19. DP-2/LCS.cpp
+++ 19. DP-2/LCS.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 using namespace std;
 
+//it is the brute force
 int lcs(string s, string t)
 {
     if (s.size() == 0 || t.size() == 0)
@@ -18,6 +19,7 @@ int lcs(string s, string t)
     }
 }
 
+//memoisation is used to avoid un-necessary call
 int lcs_mem(string s, string t, vector<vector<int>> &dp)
 {
     int m = s.size();
@@ -44,6 +46,13 @@ int lcs_mem(string s, string t, vector<vector<int>> &dp)
     return dp[m][n];
 }
 
+//DP version
+//first we fill first row and first collumn of the the dp matrix
+//then the answer of the particual cell will be dependent on the up, left and up-diag cell.
+//if the string character matched
+//(index will be according to the size
+//(eg:-s = abc,t = dbc and we are on dp1[1,3], then it means that we are checking for the string with s of size 1 and t of size 3, so we will check if s[0] == t[2]))
+//if the char matches then add 1 + max of the above adjacent cells else just max of the cells
 int lcs_dp(string s, string t)
 {
     int m = s.size();
